e33578d98c5f7ddb9c2dcdc02dfd7a2b67729a18
stecarraa <stefano@caratozzolo.me>
2022-08-25 16:27:06 +0200

    feat: json convertor

---
+++ controller.php
@@ -0,0 +1,9 @@
+<?php
+
+include __DIR__ . '/database.php';
+
+
+
+
+header('Content-Type: application/json');
+echo json_encode($disks);
--- firstVersion.php
+++ firstVersion.php
@@ -5,33 +5,42 @@ require __DIR__ . '/database.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    
+    <link rel="stylesheet" href="./styles/style.css">
+
     <title>Document</title>
 </head>
+
 <body>
+    <header>
+        <img src="https://cdn1.iconfinder.com/data/icons/artcorepart-4/512/spotify.png" alt="">
+
+    </header>
+
+
     <main class="container">
         <div class="row">
             <?php
-            
-                foreach($disks as $disk){     
-                    echo '<div class="box">';        
-                        echo "<img src='".$disk['poster']."'>";          
-                        echo "<h2>". $disk['title']."</h2>";    
-                        echo "<div class='text'>". $disk['author']."</div>";    
-                        echo "<div class='text'>". $disk['genre']."</div>";   
-                        echo "<div class='text'>". $disk['year']."</div>"; 
-                    echo"</div>";
-                }
-                
+
+            foreach ($disks as $disk) {
+                echo '<div class="box">';
+                echo "<img src='" . $disk['poster'] . "'>";
+                echo "<h2>" . $disk['title'] . "</h2>";
+                echo "<div class='text'>" . $disk['author'] . "</div>";
+                echo "<div class='text'>" . $disk['genre'] . "</div>";
+                echo "<div class='text'>" . $disk['year'] . "</div>";
+                echo "</div>";
+            }
+
             ?>
 
         </div>
 
     </main>
 </body>
+
 </html>
